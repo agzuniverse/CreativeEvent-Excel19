6d63328d11d845dede3bd75df1d0abc8cfe4931c
Aswin G <aswinganesh666@gmail.com>
2019-11-09 22:51:09 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function display() {
   window.location.replace("itempage.html");
 }
+
+let global = 59;
+setInterval(() => {
+  document.getElementsByClassName("timer")[0].innerHTML =
+    "22:24:" + global.toString();
+  global--;
+}, 1000);
